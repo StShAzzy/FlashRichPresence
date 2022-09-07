91d354d5cd4a97c8718594039d18dfc5ae31f984
YuYu <81531703+YukaNiki@users.noreply.github.com>
2022-09-07 20:52:10 -0300

    Adiciona Time Elapsed e modifica outras coisas
    Nessa atualização fiz um Time Elapsed no Rich Presence, mas também fiz com que o Rich Presence seja só mudado quando o Flash Player for iniciado ou fechado (isso vale também quando mudar pra o Multiple Running, no caso, quando a pessoa abrir mais de um Flash Player enquanto o Rich Presence estiver rodando, vai mudar o Rich Presence).
    PS: Pelo o fato de não conseguir atualizar o Rich Presence sem ter que colocar o começo do Time Elapsed sem tirar ele, toda vez que mudar para o Multiple Running, ou de Multiple Running, ir pra Debug ou Standard, o Time Elapsed sempre vai mudar pois o Rich Presence vai ser atualizado.

--- main.py
+++ main.py
@@ -3,11 +3,12 @@ if sys.platform != "win32":
     print("This program is only for Windows")
     sys.exit(1)
 import psutil
-from pypresence import Presence
+import pypresence
 import time
 import win32api
 import getfileprops
 import verifyfpprocess
+import updaterichpresence
 
 if __name__ == "__main__":
     #fp means flashplayer, fpd means flashplayerdebug
@@ -16,17 +17,29 @@ if __name__ == "__main__":
     already_verified_fps = [False, False] # This is for verifying if verifyfpprocess was already ran or not for flashplayer debug or flashplayer standard.
     fpdinfo = None # The variable for information from flashplayer debug, such as if it's running(true or false) or the flash player debug path.
     fpinfo = None # # The same as the variable above, but for flash player standard.
+    last_verifyfps = [None, None]
     type = "Unknown" # The type of flash player, standard or debug mode.
     version = "Unknown" # The variable to save the flash player version.
-  
+    first_runfp = False
+    first_runfpd = False
     CLIENT_ID = '1013717821122936873' # Don't change that, please.
-    RPC = Presence(CLIENT_ID) # Client ID connect
-
+    try:
+        RPC = pypresence.Presence(CLIENT_ID) # Client ID connect
+    except pypresence.exceptions.DiscordNotFound:
+        print("Your Discord is not running or not installed!")
+        sys.exit(1)
     while True:
         time.sleep(1)
-        
+        try:
+            last_verifyfps[0] = fpinfo[0]
+        except TypeError:
+            last_verifyfps[0] = None
+        try:
+            last_verifyfps[1] = fpdinfo[0]
+        except TypeError:
+            last_verifyfps[1] = None
         if already_verified_fps[0] == False: # This verify if verifyfpprocess already ran to get the flashplayer(standard) info, if it has been already ran, so it won't run again. 
-            fpinfo = verifyfpprocess.verifyfpprocess("S") 
+            fpinfo = verifyfpprocess.verifyfpprocess("S")
         if already_verified_fps[1] == False:
             fpdinfo = verifyfpprocess.verifyfpprocess("D")
 
@@ -50,25 +63,43 @@ if __name__ == "__main__":
                 rpcrun = False
                 already_verified_fps[0] = False
                 already_verified_fps[1] = False
-            continue
+            continue  
         if rpcrun == False:
             RPC.connect()
             rpcrun = True
             if fpinfo[0]:
                 already_verified_fps[0] = True
-            elif fpdinfo[0]:
+                first_runfp = True
+            if fpdinfo[0]:
                 already_verified_fps[1] = True
-        if fpdinfo[0] and fpinfo[0]:
+                first_runfpd = True
+
+        if (last_verifyfps[0] == False and last_verifyfps[1] == True) and (fpinfo[0] and fpdinfo[0]):
+            first_runfp = True
+            first_runfpd = True
+        elif last_verifyfps[0] == True and fpinfo[0] == False:
+            first_runfpd = True
+        if (last_verifyfps[1] == False and last_verifyfps[0] == True) and (fpdinfo[0] and fpinfo[0]):
+            first_runfpd = True
+            first_runfp = True
+        elif last_verifyfps[1] == True and fpdinfo[0] == False:
+            first_runfp = True
+        if (fpdinfo[0] and fpinfo[0]) and (first_runfp and first_runfpd):
             print("Found Debug and Standard Build")      
-            version = f"Multiple Running" 
-            type = "Multiple Running" 
-        elif fpdinfo[0]:
+            version = "Multiple Running" 
+            type = "Multiple Running"
+            updaterichpresence.updatepresence(RPC, version, type)
+            first_runfp = False
+            first_runfpd = False
+        elif fpdinfo[0] and first_runfpd:
             print("Found Debug Build")
             version = filepropsfpd['FileVersion']
             type = "Debug"
-        elif fpinfo[0]:
+            updaterichpresence.updatepresence(RPC, version, type)
+            first_runfpd = False
+        elif fpinfo[0] and first_runfp:
             print("Found Standard Build")
             version = filepropsfp['FileVersion']
             type = "Standard"
-        if rpcrun == True:
-           RPC.update(state="Idle", details="Not Quite Dead Yet.", large_image="afp32_big", large_text=f"Version {version} ({type})")
+            updaterichpresence.updatepresence(RPC, version, type)
+            first_runfp = False
